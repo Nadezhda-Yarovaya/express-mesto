d833735b8ba6158a28d8f5fa3e087dc6e47ac3e0
Nadezhda-Yarovaya <seriozz@yandex.ru>
2022-02-15 13:24:08 +0300

    1502 readme

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const router = require("./router");
+const router = require("./routes");
 
 const { PORT = 3000 } = process.env;
 
